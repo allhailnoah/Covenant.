d272a9587dd9a50f2b048a3638a2b7e543da3a50
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-10-26 20:04:37 +0200

    Adding conf <3

---
+++ conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+    t.title = "Covenant."
+    t.author = "Team Fimichi"
+    t.console = false
+    t.version = "0.8.0"
+    t.screen.width = 800
+    t.screen.height = 600
+    t.modules.joystick = false
+    t.modules.physics = false
+	t.screen.fullscreen = false
+end
