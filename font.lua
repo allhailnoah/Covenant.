fdb66b1a84e85ec81acc959657c90315f65ef164
Michał Sidor <michcioperz@autistici.org>
2014-01-04 00:41:09 +0100

    Updated font system to allow different kinds of fonts

--- font.lua
+++ font.lua
@@ -1,14 +1,27 @@
 local fonts = {}
 fonts.storage = {}
-function fonts:get(size)
-	if fonts.storage[size] then
-
+function fonts:get(type, size)
+	if fonts.storage[type] then
+		if fonts.storage[type][size] then
+			return fonts.storage[type][size]
+		else
+			fonts.storage[type][size] = fonts:makefont(type, size)
+			return fonts.storage[type][size]
+		end
 	else
-		fonts.storage[size] = love.graphics.newFont(size)
+		fonts.storage[type] = {}
+		fonts.storage[type][size] = fonts:makefont(type, size)
+		return fonts.storage[type][size]
 	end
-	return fonts.storage[size]
 end
-function fonts:set(size)
-	love.graphics.setFont(fonts:get(size))
+function fonts:makefont(type, size)
+	if font == 'default' or font == 'def' then
+		return love.graphics.newFont(size)
+	else
+		return love.graphics.newFont(type, size)
+	end
+end
+function fonts:set(type, size)
+	love.graphics.setFont(fonts:get(type, size))
 end
 return fonts
